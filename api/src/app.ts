b28a2860171a7c30dff425344ddca0b596216dc9
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-07-31 16:05:44 +0200

    Trying something w. cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -53,7 +53,27 @@ app.use(compression());
 // assigning app-wide cache settings
 app.use(express.static(__dirname + '/public', { maxAge: 31557600 }));
 
-app.use(cors());
+var allowedOrigins = [
+	'http://localhost:3000',
+	'http://localhost',
+	'http://bhelpful.net',
+];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin
+			// (like mobile apps or curl requests)
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.indexOf(origin) === -1) {
+				var msg =
+					'The CORS policy for this site does not ' +
+					'allow access from the specified Origin.';
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 // defining the parsed swagger file in order to be able to add to it
 var parsedSwaggerDoc = JSON.parse(JSON.stringify(swaggerDocument));
