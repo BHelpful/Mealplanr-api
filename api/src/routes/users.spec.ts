1c1630dcce300079e1606799be34c06ae389a72b
Andreas Petersen <andreasgdp@gmail.com>
2021-09-29 20:55:23 +0200

    Simplify for SO question

--- api/src/routes/users.spec.ts
+++ api/src/routes/users.spec.ts
@@ -21,22 +21,8 @@ describe('POST /users', () => {
 
 		const body = res.body;
 		expect(body?.hasOwnProperty('_id')).toBe(true);
-		expect(body?.hasOwnProperty('colectionId')).toBe(true);
-		expect(body?.hasOwnProperty('availableIngredientsId')).toBe(true);
 		expect(body?.hasOwnProperty('email')).toBe(true);
 		expect(body?.hasOwnProperty('createdAt')).toBe(true);
 		expect(body?.hasOwnProperty('updatedAt')).toBe(true);
-	});
-
-	it('Should fail as user already exists', async () => {
-		const res = await request(app).post('/users').send({
-			email: 'test@test.test',
-			password: '123456',
-			passwordconfirmation: '123456',
-		});
-
-		expect(res.body).not.toContain('_id');
-		expect(res.text).toBe('User already exists');
-		expect(res.status).toBe(409);
 	});
 });
